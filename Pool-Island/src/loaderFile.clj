614bd8275bcabde0d0abac96e273b1bd520e56d3
J. Albert Cruz <jalbertcruz@gmamil.com>
2013-08-12 13:02:51 -0400

    Corrida de experimentos para las comparaciones.

---
+++ Pool-Island/src/loaderFile.clj
@@ -0,0 +1,24 @@
+;;
+;; Author José Albert Cruz Almaguer <jalbertcruz@gmail.com>
+;; Copyright 2013 by José Albert Cruz Almaguer.
+;;
+;; This program is licensed to you under the terms of version 3 of the
+;; GNU Affero General Public License. This program is distributed WITHOUT
+;; ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+;; MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+;; AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+;;
+
+(load-file "./src/util.clj")
+(load-file "./src/protocols.clj")
+(load-file "./src/types.clj")
+(load-file "./src/factories.clj")
+(load-file "./src/problems.clj")
+
+(load-file "./src/evaluator.clj")
+(load-file "./src/islandManager.clj")
+(load-file "./src/poolManager.clj")
+(load-file "./src/profiler.clj")
+(load-file "./src/manager.clj")
+(load-file "./src/reproducer.clj")
+(load-file "./src/experiment.clj")
